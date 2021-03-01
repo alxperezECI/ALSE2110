a6bf0910ede35d888ee2d09691d3259d2d15f1cd
alxperezECI <alexander.perez@escuelaing.edu.co>
2021-03-01 15:25:31 -0500

    Agregando un filtro de señales.

---
+++ ex7/filtro_lista_stl/Filtro.cpp
@@ -0,0 +1,46 @@
+/**
+ * Project Untitled
+ */
+
+
+#include "Filtro.h"
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Filtro implementation
+ */
+Filtro::Filtro(int tamano){
+    _tamano = tamano;
+}
+
+/**
+ * @param a
+ * @return void
+ */
+void Filtro::agregarDato(float a) {
+    if( _datos.size() == _tamano )
+        _datos.pop_back();
+
+    _datos.push_front( a );
+    return;
+}
+
+/**
+ * @return float
+ */
+float Filtro::promedio() {
+    float prom =0.;
+    std::list<float>::iterator it;
+    if( _datos.empty() ) return 0.;
+    for( it = _datos.begin(); it != _datos.end(); ++it )
+        prom += *it;
+
+    prom /= _datos.size();
+    return prom;
+}
+
+
+
